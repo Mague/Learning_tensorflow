c9924fe2853d13a42c83f08567032fc73d1c020d
Mague <enmanueldavidmolina@gmail.com>
2017-08-14 19:12:51 -0400

    uso de un modelo basico,variables y funcion de perdida

--- basic.py
+++ basic.py
@@ -1,3 +1,5 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
 node1 = tf.constant(3.0,dtype=tf.float32)
@@ -11,3 +13,12 @@ print("sess.run(node3):",sess.run(node3))
 node4 = tf.add(node3,node3)
 print("node4: ",node4)
 print("sess.run(node4):",sess.run(node4))
+
+# Marcador de posicion
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b # es igual a tf.add(a,b)
+print(sess.run(adder_node,{a:11,b:15.9}))
+print(sess.run(adder_node,{a:[11,12],b:[15.9,12]}))
+add_and_triple = adder_node * 3
+print(sess.run(add_and_triple,{a:11,b:15.9}))
---
+++ vars.py
@@ -0,0 +1,26 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+w = tf.Variable([.3],dtype=tf.float32)
+b = tf.Variable([-.3],dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+
+linear_model = w * x + b #modelo
+
+sess = tf.Session()
+""" init es un identificador para el subgrafico tf 
+	que inicializa todas las variables
+"""
+init = tf.global_variables_initializer()
+sess.run(init)
+print(sess.run(linear_model,{x:[1,2,3,4]}))
+y = tf.placeholder(tf.float32)
+
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss,{x:[1,2,3,4], y:[0,-1,-2,-3]}))
+fixW = tf.assign(w,[-1.])
+fixb = tf.assign(b,[1.])
+sess.run([fixW,fixb])
+print(sess.run(loss,{x:[1,2,3,4], y:[0,-1,-2,-3]}))
